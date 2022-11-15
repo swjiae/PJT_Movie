806b7c1187366801fcbf4d09b44b3c60efc0e3fc
swjiae <jiae1625@gmail.com>
2022-11-15 22:31:24 +0900

    Feat: get data
    get sample data from TMDB
    make sample movie model

--- back-server/movies/models.py
+++ back-server/movies/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Movie(models.Model):
+    movie_id = models.IntegerField()
+    title = models.CharField(max_length=100)
+    released_date = models.DateTimeField()
+    popularity = models.IntegerField()
+    vote_avg = models.IntegerField()
+    overview = models.TextField()
+    poster_path = models.CharField(max_length=100)
+    genres = models.TextField()
