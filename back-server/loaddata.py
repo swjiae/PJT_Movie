c0c882c9140417a461f3e96641dc26b825a75619
congmaru <issuecap8@naver.com>
2022-11-16 17:57:25 +0900

    data upload

--- back-server/loaddata.py
+++ back-server/loaddata.py
@@ -3,6 +3,8 @@ import json
 
 TMDB_API_KEY = '7ea1fc4849eb65068cdc07be0395d96c'
 
+movie_id=[]
+
 def get_movie_datas():
     datas = []
 
@@ -30,7 +32,68 @@ def get_movie_datas():
                 }
 
                 datas.append(data)
+                movie_id.append(data['pk'])
+                # print(data['pk'])
+
     with open("movie_data.json", "w", encoding="utf-8") as w:
+        json.dump(datas, w, indent="\t", ensure_ascii=False)
+
+get_movie_datas()
+
+def get_credit_datas():
+    datas=[]
+
+    for i in range(50):
+        URL = f"https://api.themoviedb.org/3/movie/{movie_id[i]}/credits?api_key={TMDB_API_KEY}&language=ko-KR"
+        actors = requests.get(URL).json()
+
+        for actor in actors['cast'][:5]:
+            if actor.get('id', ''):
+                fields = {
+                    'gender': actor['gender'],
+                    'name': actor['name'],
+                    'popularity': actor['popularity'],
+                    'profile_path': actor['profile_path'],
+                    'character': actor['character'],
+                    "movie_id": actors['id'],
+                }
+
+                data = {
+                    "model": "movies.actor",
+                    "fields": fields
+                }
+
+                datas.append(data)
+    
+    with open("credit_data.json", "w", encoding="utf-8") as w:
+        json.dump(datas, w, indent="\t", ensure_ascii=False)
+
+get_credit_datas()
+
+def get_movie_datas():
+    datas = []
+
+    URL = f"https://api.themoviedb.org/3/genre/movie/list?api_key={TMDB_API_KEY}&language=ko-KR&page"
+    genres = requests.get(URL).json()
+
+    for genre in genres['genres']:
+      # print(genre)
+      if genre.get('id', ''):
+          fields = {
+              'genre_id': genre['id'],
+              'name': genre['name'],
+          }
+
+          data = {
+              "pk": genre['id'],
+              "model": "movies.genre",
+              "fields": fields
+          }
+          datas.append(data)
+
+
+      
+    with open("genre_data.json", "w", encoding="utf-8") as w:
         json.dump(datas, w, indent="\t", ensure_ascii=False)
 
 get_movie_datas()
--- back-server/movies/models.py
+++ back-server/movies/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Genre(models.Model):
+    genre_id = models.IntegerField()
+    name = models.CharField(max_length=50)
+
 class Movie(models.Model):
     movie_id = models.IntegerField()
     title = models.CharField(max_length=100)
@@ -9,4 +13,13 @@ class Movie(models.Model):
     vote_avg = models.IntegerField()
     overview = models.TextField()
     poster_path = models.CharField(max_length=100)
-    genres = models.TextField()
+    genres = models.ManyToManyField(Genre, related_name='genres')
+
+class Actor(models.Model):
+    actor_pk = models.AutoField(primary_key=True)
+    gender = models.IntegerField()
+    name = models.CharField(max_length=50)
+    popularity = models.IntegerField()
+    profile_path = models.TextField(null=True)
+    character = models.CharField(max_length=100)
+    movie_id = models.IntegerField()
