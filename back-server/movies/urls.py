f34a786b57ba1f59687259958bce09f76b2f2e92
congmaru <issuecap8@gmail.com>
2022-11-23 22:24:22 +0900

    trailer data update

--- back-server/movies/urls.py
+++ back-server/movies/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('movies/', views.movie_list),
     path('movies/<int:movie_pk>/', views.movie_detail),
     path('credits/<int:movie_pk>/', views.movie_credits),
+    path('trailer/<int:movie_pk>/', views.movie_trailer),
     path('movies/<int:movie_pk>/reviews/', views.review_create),
     path('reviews/', views.review_list),
     path('reviews/<int:review_pk>', views.review_detail),
