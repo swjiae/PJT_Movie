9473fe80ebb14fbcb41b796cfcde387cde282979
congmaru <issuecap8@gmail.com>
2022-11-24 00:14:34 +0900

    video hover

--- front-server/src/views/MovieDetailView.vue
+++ front-server/src/views/MovieDetailView.vue
@@ -7,33 +7,42 @@
             <b-card-img :src="url+movie.poster_path" alt="Image"
             class="rounded-0"
             style="height: auto"
+            @mouseover="activate"
+            @mouseout="diactivate"
+            v-b-modal.modal_trailer
             ></b-card-img>
           </b-col>
           <b-col md="5">
-            <b-card-body :title="movie.title">
-              <b-card-text>
-                {{movie.id}}
+            <b-card-body :title="movie.title" class="d-flex row">
+              <hr>
+              <!-- 텍스트 일렬정렬하기 -->
+              <div>
+                <b-card-text>
+                <li>{{movie.id}}</li>
                 <li>평점 : {{movie.vote_avg}}</li>
                 <li>개봉일 : {{movie.released_date}}</li>
                 <li>장르 : {{genres}}</li>
                 <li>줄거리 : {{movie.overview}}</li>
-      {{trailer_url}}
               </b-card-text>
+              </div>
             </b-card-body>
           </b-col>
         </b-row>
       </b-card>
     </div>
     <!-- 동영상팝업창으로 넣기 -->
-    <div>
-        <b-embed
+    <div
+    v-show="isShow"
+    id="trailer">
+    <b-embed
           type="iframe"
           aspect="16by9"
           :src="trailer_url"
           allowfullscreen
+          width="80%" height="800px"
         ></b-embed>
+    </div>
 
-      </div>
       <hr>
       <MovieReviewList :movie="movie"/>
     </div>
@@ -44,6 +53,7 @@
 <script>
 import MovieReviewList from '@/components/MovieReviewList'
 import axios from 'axios'
+
 const API_URL = 'http://127.0.0.1:8000'
 export default {
     name: 'MovieDetailView',
@@ -61,6 +71,7 @@ export default {
         trailer: null,
         trailer_key:null,
         trailer_url: null,
+        isShow: false,
       }},
     created() {
       this.getAllDetail()
@@ -98,9 +109,20 @@ export default {
             this.genres.push(el.name)
           })
         },
+        activate() {
+          this.isShow = true
+        },
+        diactivate() {
+          this.isShow = false
+        },
     },
 }
 </script>
 
 <style>
+#trailer {
+  position:absolute;
+  z-index: 2;
+}
+
 </style>
