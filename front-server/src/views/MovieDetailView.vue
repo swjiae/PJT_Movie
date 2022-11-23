7626017d9db89ca72dc5dd559d320944e1c5e2f4
congmaru <issuecap8@gmail.com>
2022-11-23 22:23:55 +0900

    layout detail + a

--- front-server/src/views/MovieDetailView.vue
+++ front-server/src/views/MovieDetailView.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="row">
-    <!-- {{ movie }} -->
     <div class="d-flex justify-content-center">
       <b-card no-body class="overflow-hidden" style="max-width: 50%; height:800px">
         <b-row no-gutters>
@@ -8,28 +7,36 @@
             <b-card-img :src="url+movie.poster_path" alt="Image"
             class="rounded-0"
             style="height: auto"
-            @mouseover="play_preview"
             ></b-card-img>
           </b-col>
           <b-col md="5">
             <b-card-body :title="movie.title">
               <b-card-text>
+                {{movie.id}}
                 <li>평점 : {{movie.vote_avg}}</li>
                 <li>개봉일 : {{movie.released_date}}</li>
-                <!-- <li>{{genre_list}}수정하기</li> -->
-                <li>
-                줄거리 : {{movie.overview}}
-                </li>
+                <li>장르 : {{genres}}</li>
+                <li>줄거리 : {{movie.overview}}</li>
+      {{trailer_url}}
               </b-card-text>
             </b-card-body>
           </b-col>
         </b-row>
       </b-card>
     </div>
-    <hr>
-    <MovieReviewList :movie="movie"/>
+    <!-- 동영상팝업창으로 넣기 -->
+    <div>
+        <b-embed
+          type="iframe"
+          aspect="16by9"
+          :src="trailer_url"
+          allowfullscreen
+        ></b-embed>
 
-  </div>
+      </div>
+      <hr>
+      <MovieReviewList :movie="movie"/>
+    </div>
 
 
 </template>
@@ -45,51 +52,54 @@ export default {
     },
     data() {
       return {
+        url : 'https://image.tmdb.org/t/p/original/',
+        youtube_url : 'https://www.youtube.com/embed/',
         movie: null,
         credits: [],
-        url : 'https://image.tmdb.org/t/p/original/',
-        // genre_list: [],
-        // movies: this.$store.state.movies
-        
+        genre_list: [],
+        genres: [],
+        trailer: null,
+        trailer_key:null,
+        trailer_url: null,
       }},
     created() {
-      this.getMovieDetail()
-      this.getMovieCredits()
-      this.getGenreList()
+      this.getAllDetail()
     },
+
     methods: {
-      getMovieDetail() {
-        axios({
-          method: 'get',
-          url: `${API_URL}/api/v1/movies/${this.$route.params.id}`
-        })
-          .then((res) => {
-            this.movie = res.data
+      async getAllDetail() {
+        const getdetail_res =  await axios.get(`${API_URL}/api/v1/movies/${this.$route.params.id}`)
+        const detail = getdetail_res.data
+        const genres = detail.genres
+        this.movie = detail
+        this.genre_list = genres
+        // console.log('get_movie_detail')
+        // console.log(detail)
+        // console.log('genre')
+        // console.log(this.genre_list)
+        const getcredit_res = await axios.get(`${API_URL}/api/v1/credits/${this.$route.params.id}`)
+        const credit = getcredit_res.data
+        this.credits = credit
+        // console.log('getcredit_res')
+        // console.log(credit)
+        this.test()
+        const gettrailer_res = await axios.get(`${API_URL}/api/v1/trailer/${this.$route.params.id}`)
+        // console.log('trailer')
+        this.trailer = gettrailer_res.data
+        this.trailer_key = this.trailer.key
+        const trailer_url = this.youtube_url+this.trailer_key
+        this.trailer_url = trailer_url
+        // console.log('trailer_url')
+        // console.log(this.trailer_url)
+      }
+      ,
+        test() {
+          this.genre_list.forEach((el) => {
+            this.genres.push(el.name)
           })
-          .catch((err) => {
-            console.log(err)
-          })
-      },
-      getMovieCredits() {
-        axios({
-          method: 'get',
-          url: `${API_URL}/api/v1/credits/${this.$route.params.id}`
-        })
-          .then((res) => {
-            this.credits = res.data
-            console.log(res)
-          })
-          .catch((err) => {
-            console.log(err)
-          })
-      },
-    //   getGenreList(){
-    //     console.log('movies', this.movies)
-    //     for(let i=0; i<this.movies.genres.length; i++) {
-    //   this.genre_list.push(this.movies.genres[i].name)
-    // }
-    // }
-}}
+        },
+    },
+}
 </script>
 
 <style>
